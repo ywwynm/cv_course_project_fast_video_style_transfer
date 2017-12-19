5acb3c057e6c1031872a37e605278c80d7271011
ywwynm <ywwynm@gmail.com>
2017-12-19 16:11:29 +0800

    initial commit

---
+++ frame_calculator.py
@@ -0,0 +1,16 @@
+"""
+Calculate residuals of neighbour frames for a video.
+
+Input: a folder containing frames to calculate residuals. Frames' names should be 'frame_%d.png'.
+TODO: more freedom for name template and file format.
+
+Output:
+1. An array represents the residuals:
+R[[ r0 ], [ r1 ], [ r2 ], ..., [ r3 ]], where r0 is residual between frame1 and frame2, etc.
+TODO: figure out how to represent residual as array.
+2. Residuals images in a new folder '{$video_name}_residuals' and are named in format 'residual_frame_%d_%d.png'.
+
+Usage: TODO add usage example
+"""
+
+
