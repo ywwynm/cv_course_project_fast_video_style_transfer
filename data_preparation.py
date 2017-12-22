5336cecaf5383e459e7d231c9bda5467b539290e
ywwynm <ywwynm@gmail.com>
2017-12-22 12:15:11 +0800

    fix small name issues...

--- data_preparation.py
+++ data_preparation.py
@@ -13,12 +13,12 @@ data_dir = 'input\procedure_480p_1min_wave\data'
 ori_res_train = res_calc.get_residuals(ori_dir, 0, 1200)
 trs_res_train = res_calc.get_residuals(trs_dir, 0, 1200)
 
-np.save(data_dir + '\ori_tes_train.npy', ori_res_train)
-np.save(data_dir + '\\trs_tes_train.npy', trs_res_train)
+np.save(data_dir + '\ori_res_train.npy', ori_res_train)
+np.save(data_dir + '\\trs_res_train.npy', trs_res_train)
 
 ori_res_test = res_calc.get_residuals(ori_dir, 1200, 1798)
 trs_res_test = res_calc.get_residuals(trs_dir, 1200, 1798)
 
-np.save(data_dir + '\ori_tes_test.npy', ori_res_test)
-np.save(data_dir + '\\trs_tes_test.npy', trs_res_test)
+np.save(data_dir + '\ori_res_test.npy', ori_res_test)
+np.save(data_dir + '\\trs_res_test.npy', trs_res_test)
 
